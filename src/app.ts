8a27d521fa8345b91e24ccea3f1e5d0bed7d776f
Sihab Hossain <sihabhossain380@gmail.com>
2024-05-17 16:13:19 +0600

    api created for student

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+//student controller
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Student retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(id);
+
+    res.status(200).json({
+      success: true,
+      message: 'single Student retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
